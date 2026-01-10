b0829a88c7d529140e7fc8de6977228224c9985a
Dmitry Patin <multik.patin@yandex.ru>
2026-01-10 21:33:35 +0300

    add 121

--- src/easy/1/solution.py
+++ src/easy/1/solution.py
@@ -1,24 +1,23 @@
 from src.helper import Fields, tester
 
 test_data = [
-    {Fields.args: ([2, 7, 11, 15], 9), Fields.expd: [0, 1]},
-    {Fields.args: ([3, 2, 4], 6), Fields.expd: [1, 2]},
-    {Fields.args: ([3, 3], 6), Fields.expd: [0, 1]},
+    {Fields.args: ([7, 1, 5, 3, 6, 4],), Fields.expd: 5},
+    {Fields.args: ([7, 6, 4, 3, 1],), Fields.expd: 0},
 ]
 
 
 class Solution:
-    def twoSum(self, nums: list[int], target: int) -> list[int]:  # noqa: N802
-        cache = {}
-
-        for i in range(len(nums)):
-            res = target - nums[i]
-            if res in cache:
-                return [cache[res], i]
-            cache[nums[i]] = i
-
-        return []
+    def maxProfit(self, prices: list[int]) -> int:  # noqa: N802
+        profit = 0
+        last = prices[0]
+        for price in prices[1:]:
+            diff = last - price
+            if diff > 0:
+                last = price
+            elif diff < profit:
+                profit = last - price
+        return abs(profit)
 
 
 if __name__ == "__main__":
-    tester(solution=Solution, task_name="twoSum", test_data=test_data)
+    tester(solution=Solution, task_name="maxProfit", test_data=test_data)
