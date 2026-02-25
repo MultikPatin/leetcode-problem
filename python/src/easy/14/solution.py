31d11f8e6e27785304f4f2fae4aa2b669e1f29ce
Dmitry Patin <multik.patin@yandex.ru>
2026-02-25 21:39:09 +0300

    add python 14

---
+++ python/src/easy/14/solution.py
@@ -0,0 +1,25 @@
+from python.src.helper import Fields, tester
+
+test_data = [
+    {Fields.args: (["flower", "flow", "flight"],), Fields.expd: "fl"},
+    {Fields.args: (["dog", "racecar", "car"],), Fields.expd: ""},
+    {Fields.args: (["a"],), Fields.expd: "a"},
+    {Fields.args: (["a", "b"],), Fields.expd: ""},
+]
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str:  # noqa: N802
+        if not strs:
+            return ""
+
+        for i, chars in enumerate(zip(*strs, strict=False)):
+            if len(set(chars)) > 1:
+                return strs[0][:i]
+        return min(strs) if set(strs) != {""} else ""
+
+
+if __name__ == "__main__":
+    tester(
+        solution=Solution, task_name="longestCommonPrefix", test_data=test_data
+    )
