e32a7727dfceb682d75349c5b68fc12f36661633
Dmitry Patin <multik.patin@yandex.ru>
2026-02-28 22:47:20 +0300

    add python 217

---
+++ python/src/easy/217/solution.py
@@ -0,0 +1,17 @@
+from python.src.helper import Fields, tester
+
+test_data = [
+    {Fields.args: ([1, 2, 3, 1],), Fields.expd: True},
+    {Fields.args: ([1, 2, 3, 4],), Fields.expd: False},
+    {Fields.args: ([1, 1, 1, 3, 3, 4, 3, 2, 4, 2],), Fields.expd: True},
+]
+
+
+class Solution:
+    def containsDuplicate(self, nums: list[int]) -> bool:  # noqa: N802
+        return len(set(nums)) != len(nums)
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    tester(func=solution.containsDuplicate, test_data=test_data)
