054023d8714d5e3ca09273318a42721370a8160d
Dmitry Patin <multik.patin@yandex.ru>
2026-02-28 22:51:16 +0300

    add python 219

---
+++ python/src/easy/219/solution.py
@@ -0,0 +1,22 @@
+from python.src.helper import Fields, tester
+
+test_data = [
+    {Fields.args: ([1, 2, 3, 1], 3), Fields.expd: True},
+    {Fields.args: ([1, 0, 1, 1], 1), Fields.expd: True},
+    {Fields.args: ([1, 2, 3, 1, 2, 3], 2), Fields.expd: False},
+]
+
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: list[int], k: int) -> bool:  # noqa: N802
+        last = {}
+        for i, num in enumerate(nums):
+            if num in last and abs(i - last[num]) <= k:
+                return True
+            last[num] = i
+        return False
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    tester(func=solution.containsNearbyDuplicate, test_data=test_data)
