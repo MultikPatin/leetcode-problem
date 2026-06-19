0729d50767aa9e67ecc3b2a47e9f8facf7afa959
Dmitry Patin <multik.patin@yandex.ru>
2026-06-19 22:59:21 +0300

    Create stack.go

---
+++ go/data_structures/stack/stack.go
@@ -0,0 +1,36 @@
+package stack
+
+type Stack struct {
+	items []string
+}
+
+func NewStack() *Stack {
+	return &Stack{items: []string{}}
+}
+
+func (s *Stack) IsEmpty() bool {
+	return len(s.items) == 0
+}
+
+func (s *Stack) lastIndex() int {
+	return s.Size() - 1
+}
+
+func (s *Stack) Size() int {
+	return len(s.items)
+}
+
+func (s *Stack) Push(item string) {
+	s.items = append(s.items, item)
+}
+
+func (s *Stack) Pop() string {
+	i := s.lastIndex()
+	lastItem := s.items[i]
+	s.items = s.items[:i]
+	return lastItem
+}
+
+func (s *Stack) Peek() string {
+	return s.items[s.lastIndex()]
+}
