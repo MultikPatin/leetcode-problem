bbf44ba35336b2f7d54363a04a862664e97a047d
Dmitry Patin <multik.patin@yandex.ru>
2026-06-26 14:49:14 +0300

    add algorithms go binarySearch

---
+++ go/algorithms/search/binary_search.go
@@ -0,0 +1,20 @@
+package search
+
+func binarySearch(arr []int, x int, left int, right int) int {
+	if right <= left {
+		// промежуток пуст
+		return -1
+	}
+	// промежуток не пуст
+	mid := (left + right) / 2
+	if arr[mid] == x {
+		// центральный элемент — искомый
+		return mid
+	} else if x < arr[mid] {
+		// искомый элемент меньше центрального значит следует искать в левой половине
+		return binarySearch(arr, x, left, mid)
+	} else {
+		// иначе следует искать в правой половине
+		return binarySearch(arr, x, mid+1, right)
+	}
+}
