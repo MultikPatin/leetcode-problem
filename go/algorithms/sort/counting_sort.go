e01f1535dc7faa4ecf6370a258df2f3e7af6eb57
Dmitry Patin <multik.patin@yandex.ru>
2026-06-28 20:03:54 +0300

    add go countingSort

---
+++ go/algorithms/sort/counting_sort.go
@@ -0,0 +1,17 @@
+package sort
+
+func countingSort(array []int, k int) []int {
+	countedValues := make([]int, k)
+	for _, value := range array {
+		countedValues[value]++
+	}
+
+	index := 0
+	for value := 0; value < k; value++ {
+		for amount := 0; amount < countedValues[value]; amount++ {
+			array[index] = value
+			index++
+		}
+	}
+	return array
+}
