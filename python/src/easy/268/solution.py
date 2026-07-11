428850fd01ab727521cb7b3b171d19b524a06563
Dmitry Patin <multik.patin@yandex.ru>
2026-07-11 17:01:56 +0300

    fix python 268

--- python/src/easy/268/solution.py
+++ python/src/easy/268/solution.py
@@ -1,20 +1,23 @@
 from src.helper import Fields, tester
 
 test_data = [
-    {Fields.args: ([3,0,1],), Fields.expd: 2},
-    {Fields.args: ([0,1],), Fields.expd: 2},
-    {Fields.args: ([9,6,4,2,3,5,7,0,1],), Fields.expd: 8},
+    {Fields.args: ([3, 0, 1],), Fields.expd: 2},
+    {Fields.args: ([0, 1],), Fields.expd: 2},
+    {Fields.args: ([9, 6, 4, 2, 3, 5, 7, 0, 1],), Fields.expd: 8},
 ]
 
 
 class Solution:
     def missingNumber(self, nums: list[int]) -> int:  # noqa: N802
-        m = len(nums)
-        nums.sort()
-        for i in range(m):
-            if nums[i] != i:
-                return i
-        return m
+        return sum(range(len(nums) + 1)) - sum(nums)
+
+    # def missingNumber(self, nums: list[int]) -> int:  # noqa: N802
+    #     m = len(nums)
+    #     nums.sort()
+    #     for i in range(m):
+    #         if nums[i] != i:
+    #             return i
+    #     return m
 
 
 if __name__ == "__main__":
